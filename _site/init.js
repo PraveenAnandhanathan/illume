ce873f8d85f364fb3b48c21deedba69867c976c5
Praveen Anandhanathan <prave.anand124@@gmail.com>
2026-04-24 00:52:08 +0530

    MV3 CSP fix: move inline script to init.js, remove default_popup from manifest

---
+++ _site/init.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+	var base_uri;
+	if(location.href.indexOf("/_plugin/") !== -1) {
+		base_uri = location.href.replace(/_plugin\/.*/, '');
+	}
+	var args = location.search.substring(1).split("&").reduce(function(r, p) {
+		r[decodeURIComponent(p.split("=")[0])] = decodeURIComponent(p.split("=")[1]); return r;
+	}, {});
+	new app.App("body", {
+		id: "es",
+		base_uri: args["base_uri"] || base_uri,
+		auth_user : args["auth_user"] || "",
+		auth_password : args["auth_password"],
+		dashboard: args["dashboard"]
+	});
+};
